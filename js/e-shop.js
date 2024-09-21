42a0d797530bdcc474b306faede45d4412a97f6c
Blue-V <blue-v@Blue-Vs-iMac.local>
2024-09-21 16:04:26 +0400

    forEach added

--- js/e-shop.js
+++ js/e-shop.js
@@ -1,15 +1,4 @@
-// import { products } from "./data.js";
-
-const newH3 = document.createElement("h3");
-
-newH3.textContent = "Other Products!"
-newH3.id = "description"
-
-
-
-const item3 = document.querySelectorAll(".items3");
-document.body.insertBefore(newH3, item3[0]);
-document.getElementById('items3').style.display = 'flex';
+import { products } from "./data.js";
 
 // document.body.append(newH3);
 
@@ -18,52 +7,52 @@ document.getElementById('items3').style.display = 'flex';
 // document.body.append("img")
 
 // item 1
-let res = document.getElementById('card');
+// let res = document.getElementById('card');
 
-             card = document.getElementById('card');
-            let img = document.createElement('img');
-            img.src = '../e-shop/images/product1.jpeg';
-            card.append(img);
-            img.classList.add('card-img');
+//              card = document.getElementById('card');
+//             let img = document.createElement('img');
+//             img.src = '../e-shop/images/product1.jpeg';
+//             card.append(img);
+//             img.classList.add('card-img');
 // // item 2
 
-let pes = document.createElement('div');
-pes.classList.add('card');
-pes.setAttribute('id', 'card2')
-document.body.append(pes);
+// let pes = document.createElement('div');
+// pes.classList.add('card');
+// pes.setAttribute('id', 'card2')
+// document.body.append(pes);
 
     
-    let tes = document.getElementById('card2')
-    const img2 = document.createElement('img');
-    img2.src = '../e-shop/images/product2.jpeg';
-    tes.append(img2);
-    img2.classList.add('card-img');
+//     let tes = document.getElementById('card2')
+//     const img2 = document.createElement('img');
+//     img2.src = '../e-shop/images/product2.jpeg';
+//     tes.append(img2);
+//     img2.classList.add('card-img');
 
-let ves = document.createElement('div');
-ves.classList.add('card');
-ves.setAttribute('id', 'card3');
-document.body.append(ves);
+// let ves = document.createElement('div');
+// ves.classList.add('card');
+// ves.setAttribute('id', 'card3');
+// document.body.append(ves);
 
-let qes = document.getElementById('card3');
-const img3 = document.createElement('img');
-img3.src = '../e-shop/images/product3.jpeg';
-qes.append(img3);
-img3.classList.add('card-img');
+// let qes = document.getElementById('card3');
+// const img3 = document.createElement('img');
+// img3.src = '../e-shop/images/product3.jpeg';
+// qes.append(img3);
+// img3.classList.add('card-img');
 
 
 
-// let items4 = document.getElementById("items3");
-// console.log(products);
-// products.forEach((item) => {
-//   let card = document.createElement("div");
-//   card.classList.add('card')
-//   let img = document.createElement("img");
-//   let pTitle = document.createElement("p");
+let cards_container = document.getElementById("items3");
+console.log(products);
+products.forEach((item) => {
+  let card = document.createElement("div");
+  card.classList.add('card')
+  let img = document.createElement("img");
+  let pTitle = document.createElement("p");
 
-//   img.src = item.image;
-//   pTitle.textContent = item.title;
-//   card.appendChild(img);
-//   card.appendChild(pTitle);
-//   cards_container.appendChild(card);
-// });
+  img.src = item.image;
+  pTitle.textContent = item.title;
+  card.appendChild(img);
+  card.append(pTitle);
+  cards_container.appendChild(card);
+});
 
