fda2b59eeffeb48ff0ad005bad1b0fa1abff70ab
Ausar-Vile <ibrahimsaasa@gmail.com>
2024-10-01 13:09:25 +0400

    added cart menu

--- js/e-shop.js
+++ js/e-shop.js
@@ -1,93 +1,86 @@
-import { products } from "./data.js";
-
-// const img = document.createElement("img")
-// img.src = "../data/product1.jpeg;"
-// document.body.append("img")
-
-// item 1
-// let res = document.getElementById('card');
-
-//              card = document.getElementById('card');
-//             let img = document.createElement('img');
-//             img.src = '../e-shop/images/product1.jpeg';
-//             card.append(img);
-//             img.classList.add('card-img');
-// // item 2
-
-// let pes = document.createElement('div');
-// pes.classList.add('card');
-// pes.setAttribute('id', 'card2')
-// document.body.append(pes);
-
-    
-//     let tes = document.getElementById('card2')
-//     const img2 = document.createElement('img');
-//     img2.src = '../e-shop/images/product2.jpeg';
-//     tes.append(img2);
-//     img2.classList.add('card-img');
-
-// let ves = document.createElement('div');
-// ves.classList.add('card');
-// ves.setAttribute('id', 'card3');
-// document.body.append(ves);
-
-// let qes = document.getElementById('card3');
-// const img3 = document.createElement('img');
-// img3.src = '../e-shop/images/product3.jpeg';
-// qes.append(img3);
-// img3.classList.add('card-img');
-
-
-let cards_container = document.getElementById("items3");
-console.log(products);
-products.forEach((item) => {
-  let iTes = document.createElement('div');
-  iTes.classList.add("item");
-  let card = document.createElement("div");
-  card.classList.add('card')
-  let img = document.createElement("img");
-  let pDiv = document.createElement('div');
-  pDiv.classList.add('product-info');
-  let stars = document.createElement('div');
-  stars.classList.add('stars');
-  let starImage = document.createElement("img");
-  starImage.classList.add('star');
-  let pPrice = document.createElement('p');
-  pPrice.classList.add('price');
-  let pTitle = document.createElement("p");
-  pTitle.classList.add('product-name')
-  let btn = document.createElement('button');
-  btn.classList.add('add-cart')
+// let cards_container = document.getElementById("items3");
+// console.log(products);
+// products.forEach((item) => {
+//   let iTes = document.createElement('div');
+//   iTes.classList.add("item");
+//   let card = document.createElement("div");
+//   card.classList.add('card')
+//   let img = document.createElement("img");
+//   let pDiv = document.createElement('div');
+//   pDiv.classList.add('product-info');
+//   let stars = document.createElement('div');
+//   stars.classList.add('stars');
+//   let starImage = document.createElement("img");
+//   starImage.classList.add('star');
+//   let pPrice = document.createElement('p');
+//   pPrice.classList.add('price');
+//   let pTitle = document.createElement("p");
+//   pTitle.classList.add('product-name')
+//   let btn = document.createElement('button');
+//   btn.classList.add('add-cart')
 
   
 
 
 
-  document.getElementById('items3').appendChild(iTes);
-  img.src = item.image; 
-  starImage.src = item.stars;
-  pPrice.textContent = item.price
-  pTitle.textContent = item.title;
-  iTes.appendChild(btn);
-  btn.textContent = item.button;
-  iTes.appendChild(stars);
-  stars.appendChild(starImage);
-  card.appendChild(img);
-  img.classList.add('card-img');
-  iTes.appendChild(pDiv);
-  pDiv.prepend(pPrice);
-  pDiv.appendChild(pTitle);
-  iTes.prepend(card);
-});
+//   document.getElementById('items3').appendChild(iTes);
+//   img.src = item.image; 
+//   starImage.src = item.stars;
+//   pPrice.textContent = item.price
+//   pTitle.textContent = item.title;
+//   iTes.appendChild(btn);
+//   btn.textContent = item.button;
+//   iTes.appendChild(stars);
+//   stars.appendChild(starImage);
+//   card.appendChild(img);
+//   img.classList.add('card-img');
+//   iTes.appendChild(pDiv);
+//   pDiv.prepend(pPrice);
+//   pDiv.appendChild(pTitle);
+//   iTes.prepend(card);
+// });
 
+import { products } from "./data.js";
+import { showItems } from "./fun.js";
+console.log(products);
+const cart_icon = document.getElementById("cart_icon");
+const layout = document.querySelector(".layout");
+const model_content = document.querySelector(".content");
+const total_price = document.querySelector("#total_price");
+
+const cart = [];
+cart_icon.addEventListener("click", () => {
+  layout.classList.toggle("hide");
+  showCartItems(cart);
+});
 async function getProducts() {
   try {
-    let products = await fetch("https://fakestoreapi.com/products/1");
+    let products = await fetch("https://fakestoreapi.com/products");
     let data = await products.json();
-    console.log(data);
-    showResult(data);
+    console.log(data, "product from api");
+    showItems(data.slice(0, 3));
   } catch (error) {
-    showError("Something wrong happen in the api call");
+    // showError("Something wrong happen in the api call");
   }
 }
-console.log(getProducts());
+
+export function addToCart(item) {
+  cart.push(item);
+  console.log(cart);
+}
+
+function showCartItems(cart) {
+  let price = 0;
+
+  cart.forEach((element) => {
+    price += element.price;
+    let cart_item = document.createElement("div");
+    cart_item.textContent = element.title;
+    model_content.appendChild(cart_item);
+    total_price.textContent = price;
+    // create img
+    // create delete btn
+  });
+}
+
+getProducts();
